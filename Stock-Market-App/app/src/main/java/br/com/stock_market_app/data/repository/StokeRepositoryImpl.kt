4409d36f663a7814d04dcb209d1b02c38cad455b
Daniel Lopes <danieljfsantos@gmail.com>
2022-06-27 19:52:00 -0300

    isolando a funcionalidade de ler o arquivo CSV da api

--- Stock-Market-App/app/src/main/java/br/com/stock_market_app/data/repository/StokeRepositoryImpl.kt
+++ Stock-Market-App/app/src/main/java/br/com/stock_market_app/data/repository/StokeRepositoryImpl.kt
@@ -1,24 +1,25 @@
 package br.com.stock_market_app.data.repository
 
+import br.com.stock_market_app.data.csv.CSVParser
 import br.com.stock_market_app.data.local.StockDatabase
 import br.com.stock_market_app.data.mapper.toCompanyListing
+import br.com.stock_market_app.data.mapper.toCompanyListingEntity
 import br.com.stock_market_app.data.remote.StockApi
 import br.com.stock_market_app.domain.model.CompanyListing
 import br.com.stock_market_app.domain.repository.StokeRepository
 import br.com.stock_market_app.util.ResourceState
-import com.opencsv.CSVReader
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
-import java.io.InputStreamReader
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class StokeRepositoryImpl @Inject constructor(
     val api: StockApi,
-    val database: StockDatabase
+    val database: StockDatabase,
+    val companyListingsParser: CSVParser<CompanyListing>
 ) : StokeRepository {
 
     private val dao = database.stockDao
@@ -45,14 +46,30 @@ class StokeRepositoryImpl @Inject constructor(
         val remoteListings = try {
             val response = api.getCompanyListing()
             //para ler arquivos CSV
-            val csvReader = CSVReader(InputStreamReader(response.byteStream()))
+            companyListingsParser.parse(response.byteStream())
 
         } catch (e: IOException) {
             e.printStackTrace()
             emit(ResourceState.Error("Não foi possível carregar os dados"))
+            null
         } catch (e: HttpException) {
             e.printStackTrace()
             emit(ResourceState.Error("Não foi possível carregar os dados"))
+            null
+        }
+
+        remoteListings?.let { listings ->
+            dao.clearCompanyListings()
+            dao.insertCompanyListings(
+                listings.map { it.toCompanyListingEntity() }
+            )
+            emit(ResourceState.Success(
+                data = dao.searchCompanyListing("").map {
+                    it.toCompanyListing()
+                }
+            ))
+            emit(ResourceState.Loading(isLoading = false))
+
         }
     }
 }
